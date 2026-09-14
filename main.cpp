42dcd87686ff6068a713da2e6817f26d0ef2c45c
KolYev <evgeniy.kolesenko@gmail.com>
2026-09-14 20:09:28 +0300

    инициализация сетевого API

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
 
 using namespace std;
 
 int main()
 {
-    cout<<"Hello World!"<<endl;
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0)
+    {
+        cerr << "Failed"<< endl;
+    }
+    else {
+        cout<< "Hello World!"<<endl;
+    }
+
     return 0;
 }
